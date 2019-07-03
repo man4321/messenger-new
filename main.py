ed4ba9623c8f436f2dcd941c59eef826ec32d890
man4321 <manohar2000.seervi@gmail.com>
2019-07-03 15:10:33 +0530

    Google Authenticaton Backend

--- main.py
+++ main.py
@@ -5,104 +5,134 @@ import webapp2
 import json
 from model import*
 from methods import*
+# class Login(webapp2.RequestHandler):
+#     def post(self):
+#         #print self
+#         request=json.loads(self.request.body)
+#         #print request
+#         user=User()
+#         user.email=request.get('email')
+#         user.password=request.get('password')
+#         data=User.query(User.email==user.email , User.password==user.password).get()
+#         if (data):
+#             self.response.out.write( "successfully login")
+#             def json_convert(self):
+#                 d=[];
+#                 for i in data:
+#                     d.append({
+#                         'firstname': d.firstname,
+#                         'lastname': d.lastname,
+#                         'username': d.username,
+#                         'password': d.password,
+#                         'email': d.email
+#                         })
+#                     x=json.dumps(d)
+#                     self.response.out(x)
+
+#         else:
+#             self.response.out.write({"Error": "Incorrect email or password"})
+
+#     def get(self):
+#         pass
 
 
 
 
-class Uplode(webapp2.RequestHandler):
-    def post(self):
-        request=json.loads(self.request.body)
-        feeds=Feeds()
-        print request.get('user_key')
-        #feeds.user_key=ndb.Key(urlsafe=request.get('user_key'))
 
-        feeds.post=request.get('post')
-        feeds.put()
-        def json_convert(self):
-                d=[];
-                for i in feeds:
-                    d.append({
-                        #'user_key':d.user_key,
-                        'post':d.post
-                        })
-                self.response.out(json.dumps(d))
-    def get(self):
-        pass
+# class Uplode(webapp2.RequestHandler):
+#     def post(self):
+#         request=json.loads(self.request.body)
+#         feeds=Feeds()
+#         print request.get('user_key')
+#         #feeds.user_key=ndb.Key(urlsafe=request.get('user_key'))
 
-class MainPage(webapp2.RequestHandler):
-    feeds_per_page=10
-    def get(self):
-        #query=Feeds.query.fetch()   
-        #print query
-        cursor = Cursor(urlsafe=self.request.get('cursor'))
-        feed, next_cursor, more = Feeds.query().fetch_page(
-            self.feeds_per_page, start_cursor=cursor)
+#         feeds.post=request.get('post')
+#         feeds.put()
+#         def json_convert(self):
+#                 d=[];
+#                 for i in feeds:
+#                     d.append({
+#                         #'user_key':d.user_key,
+#                         'post':d.post
+#                         })
+#                 self.response.out(json.dumps(d))
+#     def get(self):
+#         pass
 
-        d=[];
-        for i in feed:
-            d.append({
-                'post':i.post,
-                'post_time':str(i.post_time)
+# class MainPage(webapp2.RequestHandler):
+#     feeds_per_page=10
+#     def get(self):
+#         #query=Feeds.query.fetch()   
+#         #print query
+#         cursor = Cursor(urlsafe=self.request.get('cursor'))
+#         feed, next_cursor, more = Feeds.query().fetch_page(
+#             self.feeds_per_page, start_cursor=cursor)
 
-                })
-        print d
-        self.response.out.write({"more": more,"data":json.dumps(d), "curs":next_cursor.urlsafe()})
+#         d=[];
+#         for i in feed:
+#             d.append({
+#                 'post':i.post,
+#                 'post_time':str(i.post_time)
+
+#                 })
+#         print d
+#         self.response.out.write({"more": more,"data":json.dumps(d), "curs":next_cursor.urlsafe()})
                 
-    def post(self):
-        pass
+#     def post(self):
+#         pass
 
 
-class SignUp(webapp2.RequestHandler):
-    def post(self):
-        #print self
-        request=json.loads(self.request.body)
-        #print request
-        user=User()
+# class SignUp(webapp2.RequestHandler):
+#     def post(self):
+#         #print self
+#         request=json.loads(self.request.body)
+#         #print request
+#         user=User()
 
-        user.firstname=request.get('firstname')
-        user.lastname=request.get('lastname')
-        user.username=request.get('username')
-        user.password=request.get('password')
-        user.email=request.get('email')
+#         user.firstname=request.get('firstname')
+#         user.lastname=request.get('lastname')
+#         user.username=request.get('username')
+#         user.password=request.get('password')
+#         user.email=request.get('email')
         
-        query = User.query(User.email==user.email).get()
-        if query is not None:
-            self.response.out.write({"Error": "email is already exist!"})
-        else:
-            self.response.out.write("Done")
-            user.put()
-    def get(self):
-        pass
+#         query = User.query(User.email==user.email).get()
+#         if query is not None:
+#             self.response.out.write({"Error": "email is already exist!"})
+#         else:
+#             self.response.out.write("Done")
+#             user.put()
+#     def get(self):
+#         pass
 
-class Login(webapp2.RequestHandler):
-    def post(self):
-        #print self
-        request=json.loads(self.request.body)
-        #print request
-        user=User()
-        user.email=request.get('email')
-        user.password=request.get('password')
-        data=User.query(User.email==user.email , User.password==user.password).get()
-        if (data):
-            self.response.out.write( "successfully login")
-            def json_convert(self):
-                d=[];
-                for i in data:
-                    d.append({
-                        'firstname': d.firstname,
-                        'lastname': d.lastname,
-                        'username': d.username,
-                        'password': d.password,
-                        'email': d.email
-                        })
-                    x=json.dumps(d)
-                    self.response.out(x)
+# class Login(webapp2.RequestHandler):
+#     def post(self):
+#         #print self
+#         request=json.loads(self.request.body)
+#         #print request
+#         user=User()
+#         user.email=request.get('email')
+#         user.password=request.get('password')
+#         data=User.query(User.email==user.email , User.password==user.password).get()
+#         if (data):
+#             self.response.out.write( "successfully login")
+#             def json_convert(self):
+#                 d=[];
+#                 for i in data:
+#                     d.append({
+#                         'firstname': d.firstname,
+#                         'lastname': d.lastname,
+#                         'username': d.username,
+#                         'password': d.password,
+#                         'email': d.email
+#                         })
+#                     x=json.dumps(d)
+#                     self.response.out(x)
 
-        else:
-            self.response.out.write({"Error": "Incorrect email or password"})
+#         else:
+#             self.response.out.write({"Error": "Incorrect email or password"})
 
-    def get(self):
-        pass
+#     def get(self):
+#         pass
 class Messenger_R(webapp2.RequestHandler):
     messeges_per_page=10
     def get(self):
@@ -138,10 +168,25 @@ class Messenger(webapp2.RequestHandler):
        
     def get(self):
         pass
+class Putdata(webapp2.RequestHandler):
+    def get(self):
+        #request=json.loads(self.request.body)
+        current_user=users.get_current_user()
+        user_email=current_user.email()
+        userid=current_user.user_id()
+        put_data(user_email,userid)
+        #self.redirect("/#!/chat")
+        
+        
+
+
+
+        
 
 class ListMsg(webapp2.RequestHandler):
     def post(self):
         request=json.loads(self.request.body)
+
         d=[];
         query=User.fetch().get()
         for i in query:
@@ -152,11 +197,11 @@ class ListMsg(webapp2.RequestHandler):
         self.response.out(json.dumps(d))
         
 app=webapp2.WSGIApplication([
-    ('/',Uplode),
-    ('handlers/main',MainPage),
-    ('handlers/sign',SignUp),
+    webapp2.Route('/',Putdata),
+    # ('handlers/main',MainPage),
+    # ('handlers/sign',SignUp),
     ('handlers/msg',Messenger),
-    ('handlers/login',Login),
+    # ('handlers/login',Login),
     ('handlers/reciver',Messenger_R),
     ('handlers/list',ListMsg)
     ],debug=True)
